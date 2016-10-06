2b6b02d05381266d0f46904c74751e20cb1c1a73
Fiendflug <atnr@bk.ru>
2016-10-06 10:43:24 +0300

    Поменял имя решения и проекта, добавил try/catch

--- ChangeTariff/Program.cs
+++ ChangeTariff/Program.cs
@@ -5,7 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.IO;
 
-namespace ChangeTariff
+namespace UpdatePrices
 {
     class Program
     {                
@@ -23,22 +23,31 @@ namespace ChangeTariff
 
             Console.WriteLine("Введите путь к результирующему файлу");
             string pathToTargetFile = Console.ReadLine();
-
-            FormatFileFromUTM fromUtmFile = new FormatFileFromUTM(pathToUtmFile, pathToOldFile);
-            List<string> oldComplexTariff = fromUtmFile.RunFormat();
-
-            FormatNewBeeineFile fromNewBeelineFile = new FormatNewBeeineFile(pathToNewFile);
-            Dictionary<string, string> complexNewTariff = fromNewBeelineFile.RunFormat();
-
-            foreach (KeyValuePair<string, string> kvp in complexNewTariff)
+            try
             {
-                Console.WriteLine(kvp.Key + "   " + kvp.Value);
-                //File.AppendAllText(@"ttt.csv", Environment.NewLine + kvp.Key + ";" + kvp.Value, Encoding.UTF8);                
+                FormatFileFromUTM fromUtmFile = new FormatFileFromUTM(pathToUtmFile, pathToOldFile);
+                List<string> oldComplexTariff = fromUtmFile.RunFormat();
+
+                FormatNewBeeineFile fromNewBeelineFile = new FormatNewBeeineFile(pathToNewFile);
+                Dictionary<string, string> complexNewTariff = fromNewBeelineFile.RunFormat();
+
+                foreach (KeyValuePair<string, string> kvp in complexNewTariff)
+                {
+                    Console.WriteLine(kvp.Key + "   " + kvp.Value);
+                    //File.AppendAllText(@"ttt.csv", Environment.NewLine + kvp.Key + ";" + kvp.Value, Encoding.UTF8);                
+                }
+                CompareFiles compareObject = new CompareFiles(pathToTargetFile, oldComplexTariff, complexNewTariff);
+                compareObject.CreateDifferentTarifFile();
+                Console.WriteLine("Для выхода из приложения нажмите любую клавишу");
+                Console.ReadKey();
             }
-            CompareFiles compareObject = new CompareFiles(pathToTargetFile, oldComplexTariff, complexNewTariff);
-            compareObject.CreateDifferentTarifFile();
-            Console.WriteLine("Для выхода из приложения нажмите любую клавишу");
-            Console.ReadKey();
+            catch (Exception exc)
+            {
+                Console.WriteLine(exc.Message);
+                Console.WriteLine("Для выхода из приложения нажмите любую клавишу");
+                Console.ReadKey();
+            }
+            
         }
     }
 }
